1e77c0f0bf878b21a751879cfefd57f1f1c5f0ba
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-06-29 23:52:06 +0300

    mod classes

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,12 +1,14 @@
 import huffmancoding.Frequency;
 
+import java.util.Map;
+
 public class Huffman {
     public static void main(String[] args) {
 
         String text = "abacabad";
 
         Frequency frequency = new Frequency(text);
-        System.out.println(frequency.countFrequency());
+
 
     }
 }
--- src/main/java/huffmancoding/Node.java
+++ src/main/java/huffmancoding/Node.java
@@ -1,7 +1,4 @@
 package huffmancoding;
 
 public class Node {
-
-    
-
 }
