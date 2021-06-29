78b58117f57150f95ff19e24ce6b9c55734346b7
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-06-29 13:37:57 +0300

    mod frecuency method

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,7 +1,12 @@
+import huffmancoding.Frequency;
+
 public class Huffman {
     public static void main(String[] args) {
 
         String text = "abacabad";
+
+        Frequency frequency = new Frequency(text);
+        System.out.println(frequency.countFrequency());
 
     }
 }
--- src/main/java/huffmancoding/Frequency.java
+++ src/main/java/huffmancoding/Frequency.java
@@ -1,7 +1,26 @@
 package huffmancoding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Frequency {
-    
 
+    private final String text;
 
+    public Frequency(String text) {
+        this.text = text;
+    }
+
+    public Map<Character, Integer> countFrequency() {
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            if (count.containsKey(symbol)) {
+                count.put(symbol, count.get(symbol) + 1);
+            } else {
+                count.put(symbol, 1);
+            }
+        }
+        return count;
+    }
 }
