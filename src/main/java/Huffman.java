cade00246be4a0cb288e01033e77cc45eba6f484
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-07-06 17:58:17 +0300

    some changes

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,12 +1,11 @@
 import huffmancoding.Archiver;
 import huffmancoding.CompressedFile;
-import java.io.IOException;
 
 public class Huffman {
 
     private static final String PATH = ".\\src\\main\\resources\\test.txt";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
 
         String text = "All cats are beautiful!";
