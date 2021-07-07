d5cf58137d593a89e86a22ce2c16fbc50f4a6b1a
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-07-07 12:17:00 +0300

    added HuffmanTreeNode class

--- src/main/java/files/HuffmanTreeNode.java
+++ src/main/java/files/HuffmanTreeNode.java
@@ -2,6 +2,36 @@ package files;
 
 public class HuffmanTreeNode {
 
+    private final Byte nodeByte;
+    private final int frequency;
+    private HuffmanTreeNode left;
+    private HuffmanTreeNode right;
 
+    public HuffmanTreeNode(Byte nodeByte, int frequency) {
+        this.nodeByte = nodeByte;
+        this.frequency = frequency;
+    }
 
+    public HuffmanTreeNode(Byte nodeByte, int frequency, HuffmanTreeNode left, HuffmanTreeNode right) {
+        this.nodeByte = nodeByte;
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Byte getNodeByte() {
+        return nodeByte;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public HuffmanTreeNode getLeft() {
+        return left;
+    }
+
+    public HuffmanTreeNode getRight() {
+        return right;
+    }
 }
