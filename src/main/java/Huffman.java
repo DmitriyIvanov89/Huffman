09ff6c86e4c905729d0449ff4fd08ff1ee3a5321
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-07-09 14:15:35 +0300

    aaded field file

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,12 +1,14 @@
 import huffmancoding.Archiver;
 import huffmancoding.CompressedFile;
 
+import java.io.File;
+
 public class Huffman {
 
     private static final String PATH = ".\\src\\main\\resources\\test.txt";
+    private static final File file = new File(PATH);
 
     public static void main(String[] args) {
-
 
         String text = "All cats are beautiful!";
 
