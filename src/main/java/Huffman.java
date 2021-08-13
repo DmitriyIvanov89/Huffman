625deed4de34887ec6c30b4cd4d504decfb73790
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-08-13 16:36:14 +0300

    added test4 video file

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Huffman {
 
-    private static final String PATH = ".\\src\\main\\resources\\test3.png";
+    private static final String PATH = ".\\src\\main\\resources\\test4.mkv";
     private static final File originFile = new File(PATH);
 
     public static void main(String[] args) throws IOException {
