aa0dc777c71cfe9015a1ad25be19c024fc6751c7
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-08-16 10:43:13 +0300

    fix conflicts

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Huffman {
 
-    private static final String PATH = ".\\src\\main\\resources\\test4.mkv";
+    private static final String PATH = ".\\src\\main\\resources\\test2";
     private static final File originFile = new File(PATH);
 
     public static void main(String[] args) throws IOException {
